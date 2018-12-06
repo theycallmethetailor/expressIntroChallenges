500d9fe5e28ca40cca6d6f6ac1e56674d5ef0021
Amanda <aeverett1317@gmail.com>
2018-12-06 16:17:13 -0700

    completed part 2 challenge 1

--- part2/server.js
+++ part2/server.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
-
+const fs = require('fs');
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
+app.post('/create/:name/:age', function(req, res) {
+  console.log('this worked again!');
+  let newObj = {
+    "name": req.params.name,
+    "age": req.params.age
+  };
+  // let currentStorage = fs.readFileSync('./storage.json')
+  fs.appendFileSync('./part2/storage.json', JSON.stringify(newObj))
+  res.end()
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
