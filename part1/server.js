567a3cdffdf47e8b60bafb02ee9de045c416e326
Amanda <aeverett1317@gmail.com>
2018-12-06 15:27:13 -0700

    completed 3rd challenge

--- part1/server.js
+++ part1/server.js
@@ -1,9 +1,19 @@
 var express = require('express');
+const fs = require('fs');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/hello', function(req, res) {
+  res.send("Hello!");
+});
+app.get('/', function(req, res) {
+  let indexHTMLFile = fs.readFileSync('./part1/index.html', 'utf8')
+  res.type('html');
+  res.send(indexHTMLFile)
+});
+app.post('/create/:name', function(req, res) {
+  let nameObj = {"id": 1, "name": req.params.name}
+  res.json(nameObj)
 });
 
 app.use(function(req, res) {
