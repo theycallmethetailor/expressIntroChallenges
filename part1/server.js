c449b5e4aad59a2b1a1b9185a8edc26854e73193
Amanda <aeverett1317@gmail.com>
2018-12-06 15:30:18 -0700

    completed challenge 4

--- part1/server.js
+++ part1/server.js
@@ -6,11 +6,21 @@ var port = process.env.PORT || 8000;
 app.get('/hello', function(req, res) {
   res.send("Hello!");
 });
+
 app.get('/', function(req, res) {
   let indexHTMLFile = fs.readFileSync('./part1/index.html', 'utf8')
   res.type('html');
   res.send(indexHTMLFile)
 });
+
+app.get('/verify/:age', function(req, res) {
+  if(req.params.age > 13) {
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(403)
+  }
+});
+
 app.post('/create/:name', function(req, res) {
   let nameObj = {"id": 1, "name": req.params.name}
   res.json(nameObj)
